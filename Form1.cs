f9721afce66d4dbf39d0dbac89b77f7b5c186e76
지민 최 <jimin_0128@daum.net>
2026-03-26 18:10:29 +0900

    과제4 코딩 키보드 입력 추

--- Form1.cs
+++ Form1.cs
@@ -10,6 +10,33 @@ namespace SimpleCalculator
         public Form1()
         {
             InitializeComponent();
+            this.KeyPreview = true;
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            switch (keyData)
+            {
+                case Keys.D0: case Keys.NumPad0: but_0.PerformClick(); return true;
+                case Keys.D1: case Keys.NumPad1: but_1.PerformClick(); return true;
+                case Keys.D2: case Keys.NumPad2: but_2.PerformClick(); return true;
+                case Keys.D3: case Keys.NumPad3: but_3.PerformClick(); return true;
+                case Keys.D4: case Keys.NumPad4: but_4.PerformClick(); return true;
+                case Keys.D5: case Keys.NumPad5: but_5.PerformClick(); return true;
+                case Keys.D6: case Keys.NumPad6: but_6.PerformClick(); return true;
+                case Keys.D7: case Keys.NumPad7: but_7.PerformClick(); return true;
+                case Keys.D8: case Keys.NumPad8: but_8.PerformClick(); return true;
+                case Keys.D9: case Keys.NumPad9: but_9.PerformClick(); return true;
+
+                case Keys.Add: but_pl.PerformClick(); return true;
+                case Keys.Subtract: but_mi.PerformClick(); return true;
+                case Keys.Multiply: but_mul.PerformClick(); return true;
+                case Keys.Divide: but_div.PerformClick(); return true;
+                case Keys.Enter: but_eq.PerformClick(); return true;
+                case Keys.Back: Del_but.PerformClick(); return true;
+                case Keys.Escape: C_but.PerformClick(); return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
         }
 
         private void but_0_Click(object sender, EventArgs e)
